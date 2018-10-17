ed1ea52a757c11a29b64797e9d96f8da365c809f
Andrean Januar Priatmojo <an.jp29@gmail.com>
2018-10-17 09:08:10 +0700

    fix

--- DrawingToolkit/DrawingToolkit/Tools/CircleTool.cs
+++ DrawingToolkit/DrawingToolkit/Tools/CircleTool.cs
@@ -51,19 +51,8 @@ namespace DrawingToolkit.Tools
         {
             if (e.Button == MouseButtons.Left)
             {
-                int width = 0;
-                int height = 0;
-
-                if (e.X > this.circle.cirX)
-                {
-                    width = e.X - this.circle.cirX;
-                    height = e.Y - this.circle.cirY;
-                }
-                else
-                {
-                    width = this.circle.cirX - e.X;
-                    height = this.circle.cirY - e.Y;
-                }
+                int width = e.X - this.circle.cirX;
+                int height = e.Y - this.circle.cirY;
 
                 if (width > 0 && height > 0)
                 {
@@ -77,11 +66,6 @@ namespace DrawingToolkit.Tools
         {
             if (e.Button == MouseButtons.Left)
             {
-                if (e.X <= this.circle.cirX)
-                {
-                    this.circle.cirX = e.X;
-                    this.circle.cirY = e.Y;
-                }
                 drawingCanvas.AddDrawingObject(this.circle);
             }
         }
