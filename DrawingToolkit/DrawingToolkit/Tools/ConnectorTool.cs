04e7020458cb3415b60c8f8ce7ab74b12fd1dfba
Andrean Januar Priatmojo <an.jp29@gmail.com>
2018-11-11 21:05:59 +0700

    add connector tool

---
+++ DrawingToolkit/DrawingToolkit/Tools/ConnectorTool.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using DrawingToolkit.Shape;
+
+namespace DrawingToolkit.Tools
+{
+    class ConnectorTool : ToolStripButton, Tool
+    {
+        private DrawingCanvas drawingCanvas;
+
+        public Cursor Cursor
+        {
+            get
+            {
+                return Cursors.Arrow;
+            }
+        }
+
+        public DrawingCanvas TargetCanvas
+        {
+            get
+            {
+                return this.drawingCanvas;
+            }
+
+            set
+            {
+                this.drawingCanvas = value;
+            }
+
+        }
+
+        public ConnectorTool()
+        {
+            this.Name = "Connector Tool";
+            this.ToolTipText = "Connector Tool";
+            this.Image = IconSet.connector;
+            this.CheckOnClick = true;
+        }
+
+        public void ToolMouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        public void ToolMouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        public void ToolMouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        public void ToolKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        public void ToolKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
